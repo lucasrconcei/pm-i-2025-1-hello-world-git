b1b5a02fd76c06f9d3ceccf94ebeaa40b858cf81
Lucas Rosa da Conceição <lucasrosaconceicao86@gmail.com>
2025-03-24 19:18:18 -0300

    Teste

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,19 @@
-function helloWorld() {
-    let nome = document.getElementById("nome").value;
-    alert(`Hello World: ${nome}!`);
+let elNome = document.getElementById("nome");
+let elMessage = document.getElementById("message");
+
+document.addEventListener("DOMContentLoaded",function () {
+
+    elNome.addEventListener("keyup", function (e) {
+        setMessage();
+    })
+})
+
+function setMessage() {
+
+    if (elNome.value == "") {
+        elMessage.innerText = "";
+        return;
+    }
+
+    elMessage.innerText = `Hello World: ${elNome.value}!`;
 }
